f72d8c9b57189449829be522c595d1f84279d374
Krasimir Dimitrov <133798641+krasi-pd-bg@users.noreply.github.com>
2025-12-06 17:25:24 +0200

    format main

--- main.tf
+++ main.tf
@@ -81,13 +81,13 @@ resource "azurerm_mssql_server" "sqlserver" {
 
 resource "azurerm_mssql_database" "sqldb" {
   //name                 = "db${random_integer.ri.result}"
-  name                 = "${var.sql_database_name}${random_integer.ri.result}"
-  server_id            = azurerm_mssql_server.sqlserver.id
-  collation            = "SQL_Latin1_General_CP1_CI_AS"
-  license_type         = "LicenseIncluded"
-  max_size_gb          = 2
-  sku_name             = "S0"
-  zone_redundant       = false
+  name           = "${var.sql_database_name}${random_integer.ri.result}"
+  server_id      = azurerm_mssql_server.sqlserver.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 2
+  sku_name       = "S0"
+  zone_redundant = false
   //geo_backup_enabled   = false
   storage_account_type = "Local"
 
